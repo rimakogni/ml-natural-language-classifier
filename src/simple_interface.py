01e9e89b3d53933e88525a0d281b3a1ee779aa5b
Javier Rivera Martin <javier.rivera.martin@gmail.com>
2025-08-21 15:09:54 +0100

    partial implementation of reading user msg from txt

--- src/simple_interface.py
+++ src/simple_interface.py
@@ -15,13 +15,19 @@ from src.chatbot_interface import Chatbot_Interface
 
 
 bot = Chatbot_Interface(classifier_model_path="./multiNB_model.pkl")
+user_message = []
+with open('./json_data/user_input.txt', "r", encoding="utf-8") as f:
+    for line in f:
+                line = str(line.strip())
+                if line:
+                    user_message.append(line)
 
-user_message = "My friend sent me a review of a movie. They said 'it really sucked'. I can't tell if they liked it or not!"
+# user_message = "My friend sent me a review of a movie. They said 'it really sucked'. I can't tell if they liked it or not!"
 
-reply= bot.generate_reply(user_message)
+reply= bot.generate_reply(user_message[0])
 # print(proba_row)
 print("==== User message ====")
-print(user_message)
+print(user_message[0])
 print("\n==== Bot reply ====")
 print(reply)
 #print(f' {confidence} confidence')
